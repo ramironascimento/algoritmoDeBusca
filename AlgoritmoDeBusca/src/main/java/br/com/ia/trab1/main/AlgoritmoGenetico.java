f893004a4d44b428011013239871211a35b1ce61
gilbertokoerbes <gilbertokoerbes@gmail.com>
2022-09-29 22:25:22 -0300

    implementados logs

--- AlgoritmoDeBusca/src/main/java/br/com/ia/trab1/main/AlgoritmoGenetico.java
+++ AlgoritmoDeBusca/src/main/java/br/com/ia/trab1/main/AlgoritmoGenetico.java
@@ -41,77 +41,75 @@ public class AlgoritmoGenetico {
 
     Populacao populacaoBase = new Populacao(tamPopulacao);
 
-    //seta os movmentos aleatorios para cada individuo
+    // seta os movmentos aleatorios para cada individuo
     populacaoBase.iniciaPopulacao(caminhoTotalPorIndividuo);
 
     ArrayList<Populacao> populacaoIntermediaria = new ArrayList<>();
 
-    //int n_geracoes = 3;
-    //for (int i = 0; i < n_geracoes; i++) {
+    // int n_geracoes = 3;
+    // for (int i = 0; i < n_geracoes; i++) {
     for (int i = 0; i < quantidadeDeGeracoes; i++) {
-      
+
       Populacao populacaoIntermdiariaGeracao = new Populacao(tamPopulacao);
       System.out.println("\n\n ======================================================= \n");
       System.out.println("Iniciando geracao: " + i);
       int j = 1;
 
-      //Identifica aptidao a partir do percurso no labirinto
+      // Identifica aptidao a partir do percurso no labirinto
       for (Individuo individuo : populacaoBase.getIndividuos()) {
 
-      ///System.out.println("individuo " + j++);
-        
+        System.out.println("individuo " + j++);
+
         individuo = PercorreLabirinto.PercorrerLabirinto(individuo);
         var aptidao = Aptidao(individuo);
-        
-        //System.out.println("  APTIDAO" + aptidao);
-        
+
+        // System.out.println(" APTIDAO" + aptidao);
+
         individuo.setAptidao(aptidao);
 
-        //System.out.println(individuo.toString() /*Aptidao(individuo)*/);      
+        System.out.println(individuo.toString() /* Aptidao(individuo) */);
 
       }
-      
+
       // elistismo - descobrir o melhor individuo*/
       elitismo = getElitismo(populacaoBase);
 
-      //System.out.println("ELITISMO " + this.elitismo.toString() + "Posicao" + this.elitismo.getIndexNPonto());
+      // System.out.println("ELITISMO " + this.elitismo.toString() + "Posicao" +
+      // this.elitismo.getIndexNPonto());
       System.out.println("Posicao" + this.elitismo.getIndexNPonto());
-      
-      if(elitismo.getComidasColetadas()==qntComida)
-        {
-          System.out.println("ELITISMO " + this.elitismo.toString());
-          System.out.println("SOLUCAÇÃO ENCONTRADA");
-          System.exit(0);
 
-        }
+      if (elitismo.getComidasColetadas() == qntComida) {
+        System.out.println("ELITISMO " + this.elitismo.toString());
+        System.out.println("SOLUCAÇÃO ENCONTRADA");
+        System.exit(0);
+
+      }
 
       ArrayList<Individuo> listaNovosIndividuos = new ArrayList<>(tamPopulacao);
       listaNovosIndividuos.add(elitismo);
-      
-      
 
-      boolean executaMutacao= true;
-      //FLUXO DE SELEÇÃO, CROSSOVER E MUTAÇÃO
-      while(listaNovosIndividuos.size() < tamPopulacao){
-          Individuo[] paiEMae = selecao(populacaoBase);
-          Individuo[] filhos = crossover(paiEMae[0], paiEMae[1]);
+      boolean executaMutacao = true;
+      // FLUXO DE SELEÇÃO, CROSSOVER E MUTAÇÃO
+      while (listaNovosIndividuos.size() < tamPopulacao) {
+        Individuo[] paiEMae = selecao(populacaoBase);
+        Individuo[] filhos = crossover(paiEMae[0], paiEMae[1]);
 
-          var moduloAuxMutacao = this.quantidadeDeGeracoes*this.taxaDeMutacaoTotal;
+        var moduloAuxMutacao = this.quantidadeDeGeracoes * this.taxaDeMutacaoTotal;
 
-          //
-          if(i%moduloAuxMutacao == 0 && executaMutacao){ //executando mutacao ao passo certo
-            filhos[new Random().nextInt(2)] = mutacao();
-            executaMutacao =false;
-          }
-          //mutacao após filhos gerados
-          
-          listaNovosIndividuos.add(filhos[0]);
-          if(listaNovosIndividuos.size() < tamPopulacao){
+        //
+        if (i % moduloAuxMutacao == 0 && executaMutacao) { // executando mutacao ao passo certo
+          filhos[new Random().nextInt(2)] = mutacao();
+          executaMutacao = false;
+        }
+        // mutacao após filhos gerados
+
+        listaNovosIndividuos.add(filhos[0]);
+        if (listaNovosIndividuos.size() < tamPopulacao) {
           listaNovosIndividuos.add(filhos[1]);
-          }
+        }
       }
       populacaoIntermdiariaGeracao.setIndividuos(listaNovosIndividuos.toArray(Individuo[]::new));
-      populacaoIntermediaria.add (populacaoIntermdiariaGeracao);
+      populacaoIntermediaria.add(populacaoIntermdiariaGeracao);
       populacaoBase = populacaoIntermdiariaGeracao;
     }
 
@@ -120,36 +118,38 @@ public class AlgoritmoGenetico {
 
   private Individuo mutacao() {
     System.out.println("***** mutando *****");
-    return new Individuo(this.caminhoTotalPorIndividuo,true); //tamanho do individuo | novos movimentos aleatorios
+    return new Individuo(this.caminhoTotalPorIndividuo, true); // tamanho do individuo | novos movimentos aleatorios
   }
 
   /**
    * 
    * @param populacao
-   * @return  pai[0] e mae[1] selecionados
+   * @return pai[0] e mae[1] selecionados
    */
-  public Individuo[] selecao(Populacao populacao){
+  public Individuo[] selecao(Populacao populacao) {
     Random gerador = new Random();
     Individuo PaieMae[] = new Individuo[2];
 
-    tamPopulacao = (populacao.getIndividuos().length -1); //-1 para evitar indexOf
-    Individuo[] pai = {populacao.getIndividuos()[gerador.nextInt(tamPopulacao)] , populacao.getIndividuos()[gerador.nextInt(tamPopulacao)]}; //seleciona dois Individuo aleatórios para o pai
-    Individuo[] mae = {populacao.getIndividuos()[gerador.nextInt(tamPopulacao)] , populacao.getIndividuos()[gerador.nextInt(tamPopulacao)]}; //seleciona dois Individuo aleatórios para o mae
+    tamPopulacao = (populacao.getIndividuos().length - 1); // -1 para evitar indexOf
+    Individuo[] pai = { populacao.getIndividuos()[gerador.nextInt(tamPopulacao)],
+        populacao.getIndividuos()[gerador.nextInt(tamPopulacao)] }; // seleciona dois Individuo aleatórios para o pai
+    Individuo[] mae = { populacao.getIndividuos()[gerador.nextInt(tamPopulacao)],
+        populacao.getIndividuos()[gerador.nextInt(tamPopulacao)] }; // seleciona dois Individuo aleatórios para o mae
 
-    //seleciona o pai com melhor aptidao
-    if(pai[0].getAptidao() > pai[1].getAptidao()) {
+    // seleciona o pai com melhor aptidao
+    if (pai[0].getAptidao() > pai[1].getAptidao()) {
       PaieMae[0] = pai[0];
-    }else{
+    } else {
       PaieMae[0] = pai[1];
     }
 
-    //seleciona o mae com melhor aptidao
-    if(mae[0].getAptidao() > mae[1].getAptidao()) {
+    // seleciona o mae com melhor aptidao
+    if (mae[0].getAptidao() > mae[1].getAptidao()) {
       PaieMae[1] = mae[0];
-    }else{
+    } else {
       PaieMae[1] = mae[1];
     }
-    
+
     return PaieMae;
   }
 
@@ -159,48 +159,46 @@ public class AlgoritmoGenetico {
    * @param mae
    * @return Individuo filhos já selecionados se são melhor
    */
-  public Individuo[] crossover(Individuo pai, Individuo mae){
+  public Individuo[] crossover(Individuo pai, Individuo mae) {
 
-      int nponto;
-      if (pai.getIndexNPonto() > mae.getIndexNPonto())
-        nponto = pai.getIndexNPonto();
-      else nponto = mae.getIndexNPonto();
+    int nponto;
+    if (pai.getIndexNPonto() > mae.getIndexNPonto())
+      nponto = pai.getIndexNPonto();
+    else
+      nponto = mae.getIndexNPonto();
 
-      Individuo[] filhos = new Individuo[2];
+    Individuo[] filhos = new Individuo[2];
 
-      Direcoes[] movimentosFilho0 = new Direcoes[pai.getMovimentosDoIndividuo().length];
-      Direcoes[] movimentosFilho1 = new Direcoes[pai.getMovimentosDoIndividuo().length];
-      
-      for (int i = 0; i < pai.getMovimentosDoIndividuo().length; i++) {
-          
-        //Crossover de cromossos | definindo n ponto como meio para cada caso
+    Direcoes[] movimentosFilho0 = new Direcoes[pai.getMovimentosDoIndividuo().length];
+    Direcoes[] movimentosFilho1 = new Direcoes[pai.getMovimentosDoIndividuo().length];
 
-          if( i < nponto ) {
-            movimentosFilho0[i] = pai.getMovimentosDoIndividuo()[i];
-            movimentosFilho1[i] = mae.getMovimentosDoIndividuo()[i];
-          }
-          else {
-            movimentosFilho0[i] = mae.getMovimentosDoIndividuo()[i];
-            movimentosFilho1[i] = pai.getMovimentosDoIndividuo()[i];
-          }
-        
+    for (int i = 0; i < pai.getMovimentosDoIndividuo().length; i++) {
+
+      // Crossover de cromossos | definindo n ponto como meio para cada caso
+
+      if (i < nponto) {
+        movimentosFilho0[i] = pai.getMovimentosDoIndividuo()[i];
+        movimentosFilho1[i] = mae.getMovimentosDoIndividuo()[i];
+      } else {
+        movimentosFilho0[i] = mae.getMovimentosDoIndividuo()[i];
+        movimentosFilho1[i] = pai.getMovimentosDoIndividuo()[i];
       }
-      filhos[0] = new Individuo(this.caminhoTotalPorIndividuo);
-      filhos[0].setMovimentosDoIndividuo(movimentosFilho0);
-      filhos[1] = new Individuo(this.caminhoTotalPorIndividuo);
-      filhos[1].setMovimentosDoIndividuo(movimentosFilho1);
-      
 
-      return filhos;
-      
+    }
+    filhos[0] = new Individuo(this.caminhoTotalPorIndividuo);
+    filhos[0].setMovimentosDoIndividuo(movimentosFilho0);
+    filhos[1] = new Individuo(this.caminhoTotalPorIndividuo);
+    filhos[1].setMovimentosDoIndividuo(movimentosFilho1);
+
+    return filhos;
 
   }
-  
+
   public Individuo getElitismo(Populacao populacao) {
     int indexOfMaxValue = -1;
     Individuo individuoComMaiorAptidao = new Individuo();
     double getMaiorAbtidao = -2;
-//    double MaxAptidao = Double.MIN_VALUE;
+    // double MaxAptidao = Double.MIN_VALUE;
     for (int i = 0; i < populacao.getIndividuos().length; i++) {
       if (populacao.getIndividuos()[i].getAptidao() > getMaiorAbtidao) {
         indexOfMaxValue = i;
@@ -225,9 +223,11 @@ public class AlgoritmoGenetico {
     // valorizamos aqueles que percorrer o menor caminho + em dobro as comidas
     // coletadas #Pode ser ser bom, ou pode ser ruim pois privilegia somente queem
     // anda pouco e nao faz nada#
-    //aptidao = (IndividuoPercorrido.getIndexNPonto()+1)+ ((50 + IndividuoPercorrido.getComidasColetadas()) == 0 ? 1 : (50 + IndividuoPercorrido.getComidasColetadas()));
+    // aptidao = (IndividuoPercorrido.getIndexNPonto()+1)+ ((50 +
+    // IndividuoPercorrido.getComidasColetadas()) == 0 ? 1 : (50 +
+    // IndividuoPercorrido.getComidasColetadas()));
 
-    aptidao = (IndividuoPercorrido.getIndexNPonto()+1) + (IndividuoPercorrido.getComidasColetadas()*50);
+    aptidao = (IndividuoPercorrido.getIndexNPonto() + 1) + (IndividuoPercorrido.getComidasColetadas() * 50);
 
     // preisamos punir ao colidir em parede ou verificar se o index do ultimo
     // movimento já ajuda
